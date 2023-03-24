e958ff500e9055885487d055018d2912b3c86adc
sanglak123 <sanghuynh.pt91@gmail.com>
2023-03-24 11:05:39 +0700

    change api url

--- data/api/configApi.js
+++ data/api/configApi.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/api";
+// const baseURL = "http://localhost:3000/api";
+const baseURL = "https://doithe247.vercel.app/api";
 
 export const rootApi = axios.create({
     baseURL: baseURL
