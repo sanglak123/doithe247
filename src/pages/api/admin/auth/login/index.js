2be0cf6953134a0aa775942bf0cf7657ca7a4ffa
sanglak123 <sanghuynh.pt91@gmail.com>
2023-03-25 11:39:40 +0700

    ut1

--- src/pages/api/admin/auth/login/index.js
+++ src/pages/api/admin/auth/login/index.js
@@ -1,24 +1,7 @@
-import { AdminAuthController, ControllAdmin } from "data/controller/admin/auth";
-import { CheckAdmin } from "data/middleware";
-import nextConnect from "next-connect";
+import { AdminAuthController } from "data/controller/admin/auth";
 
-const apiRoute = nextConnect({
-    onError(error, req, res) {
-        res
-            .status(501)
-            .json({ error: `Sorry something Happened! ${error.message}` });
-    },
-    onNoMatch(req, res) {
-        res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-    },
-});
-
-apiRoute.post(AdminAuthController.Authen.Login);
-
-export default apiRoute;
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+export default async function handler(req, res) {
+    if (req.method === "POST") {
+        await AdminAuthController.Authen.Login(req, res);
+    }
+}
