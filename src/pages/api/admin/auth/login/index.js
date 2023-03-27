e345dbab7d88e7ce2d13b31cf19c8be987f5d604
sanglak123 <sanghuynh.pt91@gmail.com>
2023-03-27 11:08:28 +0700

    ud7

--- src/pages/api/admin/auth/login/index.js
+++ src/pages/api/admin/auth/login/index.js
@@ -25,16 +25,7 @@
 import { AdminAuthController } from "data/controller/admin/auth";
 import nc from "next-connect";
 
-const handler = nc({
-    onError: (err, req, res, next) => {
-        console.error(err.stack);
-        res.status(500).end("Something broke!");
-    },
-    onNoMatch: (req, res) => {
-        res.status(404).end("Page is not found");
-    },
-})
-
+const handler = nc()
     .put(AdminAuthController.Authen.Login)
     .patch(async (req, res) => {
         throw new Error("Throws me around! Error can be caught and handled.");
